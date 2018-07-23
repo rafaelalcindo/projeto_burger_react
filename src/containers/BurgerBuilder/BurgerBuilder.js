ca80ddf444d36c06671f568cbd4ba2ac5d02a76c
rafaelalcindo <rafael_alcindo@hotmail.com>
2018-07-23 16:02:44 -0300

    Subindo BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -77,6 +77,14 @@ class BurgerBuilder extends Component {
         console.log('entrou');
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false });
+    }
+
+    purchaseContinueHandler = () => {
+        alert('You Continue!');
+    }
+
     render () {
         const disabledInfo = {
             ...this.state.ingredients
@@ -87,8 +95,12 @@ class BurgerBuilder extends Component {
         // {salad: true, meat: false, ...}
         return (
             <Aux>
-                <Modal show={this.state.purchasing} >
-                    <OrderSummary ingredients={ this.state.ingredients } />
+                <Modal show={this.state.purchasing} modalClosed={ this.purchaseCancelHandler } >
+                    <OrderSummary 
+                        ingredients={ this.state.ingredients } 
+                        price={this.state.totalPrice}
+                        purchaseCanceled={this.purchaseCancelHandler} 
+                        purchaseContinued={this.purchaseContinueHandler} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
