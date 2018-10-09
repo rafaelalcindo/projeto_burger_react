50d6ada87b788aeab836004f419e346e4bdf6dca
rafaelalcindo <rafael_alcindo@hotmail.com>
2018-10-09 18:16:36 -0300

    auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import {connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import  classes  from './Auth.css';
 import * as actions from '../../store/actions/index';
 
@@ -111,7 +112,7 @@ switchAuthModeHandler = () => {
             });
         }
 
-        const form = formElementsArray.map(formElement => (
+        let form = formElementsArray.map(formElement => (
             <Input 
                 key={formElement.id} 
                 elementType={formElement.config.elementType}
@@ -123,8 +124,21 @@ switchAuthModeHandler = () => {
                 changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ));
 
+        if(this.props.loading){
+            form = <Spinner />
+        }
+
+        let erroMessage = null;
+
+        if(this.props.error) {
+            erroMessage = (
+                <p>{ this.props.error.message }</p>
+            );
+        }
+
         return (
             <div className={classes.Auth} >
+                {erroMessage  }
                 <form onSubmit={this.submitHandler} >
                     {form}
                     <Button btnType="Success" >Submit</Button>
@@ -137,10 +151,17 @@ switchAuthModeHandler = () => {
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    }
+}
+
 const mapDispatchToProps = dispatch => {
     return {
         onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password,isSignup ))
     }
 }
 
-export default connect(null, mapDispatchToProps )(Auth);
+export default connect(mapStateToProps, mapDispatchToProps )(Auth);
--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -28,6 +28,10 @@ const authFail = (state, action) => {
     });
 }
 
+const authLogout = (state, action) => {
+    return updateObject(state, { token: null, userId: null });
+}
+
 const reducer = (state = initialState, action) => {
     switch(action.type){
         case actionTypes.AUTH_START:
@@ -35,8 +39,10 @@ const reducer = (state = initialState, action) => {
         case actionTypes.AUTH_SUCCESS:
             return authSuccess(state, action);
         case actionTypes.AUTH_FAIL: 
-            return authFail(state, action)
-        default:
+            return authFail(state, action);
+        case actionTypes.AUTH_LOGOUT:
+            return authLogout(state, action);        
+        default:    
             return state;
     }
 }
